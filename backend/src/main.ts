1864c3ca1151b7305b9f4ccd8d7058862eb81ed3
Hung Pham <hung.pham@unicornuniversity.net>
2026-05-12 18:15:43 +0200

    feat: move and fix Dockerfile and fly.toml files to root, edit gitignore files and dockerignore

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,21 +3,43 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import path from "path";
 import * as fs from "fs";
+import * as https from "node:https";
 
 async function bootstrap() {
-    const certsDir = path.join(__dirname, '..', 'certs');
+    const certsDir = path.join(process.cwd(), 'certs');
+    const httpsOptions = {
+        cert: fs.readFileSync(path.join(certsDir, 'server-cert.pem')),
+        key: fs.readFileSync(path.join(certsDir, 'server-key.pem')),
+        ca: fs.readFileSync(path.join(certsDir, 'ca-cert.pem')),
+        requestCert: true,
+        rejectUnauthorized: true,
+    };
 
-    const app = await NestFactory.create(AppModule, {
-        httpsOptions: {
-            cert:   fs.readFileSync(path.join(certsDir, 'server-cert.pem')),
-            key:    fs.readFileSync(path.join(certsDir, 'server-key.pem')),
-            ca:     fs.readFileSync(path.join(certsDir, 'ca-cert.pem')),
-            requestCert: true,
-            rejectUnauthorized: true
-        }
+    const app = await NestFactory.create(AppModule, { httpsOptions });
+
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+            forbidNonWhitelisted: true,
+            transform: true,
+        })
+    );
+
+    // Middleware / Guard for mTLS check
+    app.use((req: any, res: any, next: () => void) => {
+       if (req.url.startsWith('/api/v1/cards') || req.url.startsWith('api/v1/attendance')) {
+           if (!req.client?.authorized) {
+               return res.status(401).json({
+                   message: 'Not working or missing client certificate (mTLS)'
+               });
+           }
+
+           const cert = req.socket.getPeerCertificate();
+           console.log(`Gateway authenticated: ${cert.subject?.CN || 'unknown'}`);
+       }
+       next();
     });
-    app.useGlobalPipes(new ValidationPipe());
-    await app.listen(process.env.PORT ?? 3000);
-    console.log('mTLS server running on https://localhost:3005');
+
+    await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
